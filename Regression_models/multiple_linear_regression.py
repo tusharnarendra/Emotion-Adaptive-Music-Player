88565f92ba58ebb613501877488470b7ef1fc79f
Tushar Narendra <tushanaren@gmail.com>
2025-06-25 17:25:06 +0200

    added some comments

--- Regression_models/multiple_linear_regression.py
+++ Regression_models/multiple_linear_regression.py
@@ -4,16 +4,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
 
-
+#Load dataset and organize dependent/independent variables
 dataset = pd.read_csv('../VA_scores.csv')
 X = np.load('../normalized_features.npy')
 y = dataset.iloc[:, -2:].values
 
+#Split dataset into training and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+#Fit the model
 regressor = MultiOutputRegressor(LinearRegression())
 regressor.fit(X_train, y_train)
 
+#Generate predictions
 y_pred = regressor.predict(X_test)
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred, y_test), axis=1))
--- Regression_models/random_forest.py
+++ Regression_models/random_forest.py
@@ -5,17 +5,20 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 
+#Load dataset and organize dependent/independent variables
 dataset = pd.read_csv('../VA_scores.csv')
 X = np.load('../normalized_features.npy')
 y = dataset.iloc[:, -2:].values
 
+#Split dataset into training and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+#Fit the model
 regressor = MultiOutputRegressor(RandomForestRegressor(n_estimators=100, random_state=0))
 regressor.fit(X_train, y_train)
 
+#Generate predictions
 y_pred = regressor.predict(X_test)
-
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred, y_test), axis=1))
 
